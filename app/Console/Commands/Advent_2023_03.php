4a1126a99fbf46bb8ba4e5d3da3332d3e3b9d3b6
Martin Boer <Martini1987@gmail.com>
2023-12-08 00:17:08 +0100

    :zap: 2023-02 solution part 1 without symbol cache

--- app/Console/Commands/Advent_2023_03.php
+++ app/Console/Commands/Advent_2023_03.php
@@ -23,34 +23,20 @@ class Advent_2023_03 extends Command
 .664.598..
 TEXT;
 
-    private array $symbolList = [];
+    private array $dataLines = [];
 
     public function handle(): void
     {
         $data = $this->option('input') ?? $this->data;
-        $dataLines = explode(PHP_EOL, $data);
-        $sum = 0;
+        $this->dataLines = explode(PHP_EOL, $data);
+        $partNumberSum = 0;
 
-        // find the symbols
-        foreach ($dataLines as $index => $line) {
-            preg_match_all('/[^0-9.]/', $line, $matches, PREG_OFFSET_CAPTURE);
-
-            foreach ($matches[0] as $match) {
-                $this->symbolList[] = "$index-$match[1]";
-            }
-        }
-
-        if ($this->option('debug')) {
-            $this->comment(var_export($this->symbolList, true));
-        }
-
-        // find the numbers
-        foreach ($dataLines as $index => $line) {
+        foreach ($this->dataLines as $index => $line) {
             preg_match_all('/(\d+)/', $line, $matches, PREG_OFFSET_CAPTURE);
 
             foreach ($matches[0] as $match) {
                 $number = $match[0];
-                $isValid = $this->isValidNumberLocation($index, $match[1], strlen($number));
+                $isValid = $this->isSurroundedBy($index, $match[1], strlen($number));
 
                 if ($this->option('debug')) {
                     $this->comment($index . ' - ' . $number);
@@ -58,25 +44,23 @@ TEXT;
                 }
 
                 if ($isValid) {
-                    $sum += (int) $number;
+                    $partNumberSum += (int) $number;
                 }
             }
         }
 
-        $this->info('Part number sum: ' . $sum);
+        $this->info('Part number sum: ' . $partNumberSum);
     }
 
-    private function isValidNumberLocation(int $row, int $position, int $length): bool
+    private function isSurroundedBy(int $row, int $position, int $length): bool
     {
-        if ($this->option('debug')) {
-            $this->comment($row . ' - ' . $position . ' - ' . $length);
-        }
-
         $indexes = [$row - 1, $row, $row + 1];
 
         foreach ($indexes as $index) {
             for ($i = $position - 1; $i <= $position + $length; $i++) {
-                if (in_array("$index-" . $i, $this->symbolList)) {
+                $character = $this->dataLines[$index][$i] ?? null;
+
+                if (preg_match('/[^0-9.]/', $character)) {
                     return true;
                 }
             }
