4d9d21957f4e563fc0c9af320a99710b44281ed3
Martin Boer <Martini1987@gmail.com>
2024-12-05 22:05:37 +0100

    :sparkles: 2024-02 solution part 1

---
+++ app/Console/Commands/Advent_2024_02.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+
+class Advent_2024_02 extends Command
+{
+    protected $signature = 'advent:2024:2 {--debug} {--input=}';
+
+    protected $description = '2024 - Day 1: Red-Nosed Reports';
+
+    private string $data = <<<TEXT
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+TEXT;
+
+    public function handle(): void
+    {
+        $data = $this->option('input') ?? $this->data;
+        $dataLines = explode(PHP_EOL, $data);
+        $safeReportCount = 0;
+
+        foreach ($dataLines as $line) {
+            $report = explode(' ', $line);
+
+            if ($this->isSafe($report)) {
+                $safeReportCount++;
+            }
+        }
+
+        $this->info('Number of safe reports: ' . $safeReportCount);
+    }
+
+    private function isSafe(array $report): bool
+    {
+        try {
+            if ($report[0] > $report[1]) {
+                return $this->isAllDecreasing($report) && $this->differenceAllowed($report);
+            }
+            if ($report[0] < $report[1]) {
+                return $this->isAllIncreasing($report) && $this->differenceAllowed($report);
+            }
+        } catch (Exception) {
+        }
+
+        return false;
+    }
+
+    private function isAllDecreasing(array $report): bool
+    {
+        return array_reduce($report, function ($carry, $item) {
+            if ($carry !== null && $carry <= $item) {
+                throw new Exception('Nope');
+            }
+
+            return $item;
+        });
+    }
+
+    private function isAllIncreasing(array $report): bool
+    {
+        return array_reduce($report, function ($carry, $item) {
+            if ($carry !== null && $carry >= $item) {
+                throw new Exception('Nope');
+            }
+
+            return $item;
+        });
+    }
+
+    private function differenceAllowed(array $report): bool
+    {
+        return array_reduce($report, function ($carry, $item) {
+            if ($carry !== null && max($carry, $item) - min($carry, $item) > 3) {
+                throw new Exception('Nope');
+            }
+
+            return $item;
+        });
+    }
+}
