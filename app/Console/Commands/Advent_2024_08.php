31c89e214d7ab76c8cccc17f7f4cdc31a11b659a
Martin Boer <Martini1987@gmail.com>
2024-12-13 11:12:34 +0100

    :sparkles: 2024-08 solution part 2

--- app/Console/Commands/Advent_2024_08.php
+++ app/Console/Commands/Advent_2024_08.php
@@ -25,14 +25,18 @@ class Advent_2024_08 extends Command
 ............
 TEXT;
 
+    private array $antinodes1 = [];
+    private array $antinodes2 = [];
+    private int $maxX = 0;
+    private int $maxY = 0;
+
     public function handle(): void
     {
         $data = $this->option('stdin') ? file_get_contents('php://stdin') : $this->data;
         $dataLines = explode(PHP_EOL, $data);
         $dataMatrix = collect($dataLines)->map(fn ($line) => str_split($line))->toArray();
-        $maxX = count($dataMatrix[0]) - 1;
-        $maxY = count($dataMatrix) - 1;
-        $antinodes = [];
+        $this->maxX = count($dataMatrix[0]) - 1;
+        $this->maxY = count($dataMatrix) - 1;
 
         foreach ($dataMatrix as $antennaX => $antennaLine) {
             foreach ($antennaLine as $antennaY => $antennaFrequency) {
@@ -52,32 +56,37 @@ TEXT;
                         }
 
                         // Take the first antenna and extend the X / Y distance from the second antenna.
-                        $newX = $this->extendCoordinate($antennaX, $x);
-                        $newY = $this->extendCoordinate($antennaY, $y);
-                        if ($newX >= 0 && $newX <= $maxX && $newY >= 0 && $newY <= $maxY) {
-                            $antinodes[] = $newX . '_' . $newY;
-                        }
+                        $this->findAntinode($antennaX, $antennaY, $x, $y);
 
                         // Take the second antenna and extend the X / Y distance from the first antenna.
-                        $newX = $this->extendCoordinate($x, $antennaX);
-                        $newY = $this->extendCoordinate($y, $antennaY);
-                        if ($newX >= 0 && $newX <= $maxX && $newY >= 0 && $newY <= $maxY) {
-                            $antinodes[] = $newX . '_' . $newY;
-                        }
+                        $this->findAntinode($x, $y, $antennaX, $antennaY);
                     }
                 }
             }
         }
 
-        $this->info('Unique locations containing an antinode: ' . count(array_unique($antinodes)));
+        $this->info('Unique antinode locations: ' . count(array_unique($this->antinodes1)));
+        $this->info('Unique antinode locations using resonant harmonics: ' . count(array_unique($this->antinodes2)));
     }
 
-    private function extendCoordinate($a, $b): int
+    private function findAntinode(string $x1, string $y1, string $x2, string $y2, int $depth = 0): void
     {
-        return match (true) {
-            $a === $b => $a,
-            $a < $b   => $a - ($b - $a),
-            $a > $b   => $a + ($a - $b),
-        };
+        // Antennas are always antinodes when using resonant harmonics.
+        $this->antinodes2[] = $x1 . '_' . $y1;
+
+        $deltaX = $x2 - $x1;
+        $deltaY = $y2 - $y1;
+
+        $newX = $x1 - $deltaX;
+        $newY = $y1 - $deltaY;
+
+        if ($newX >= 0 && $newX <= $this->maxX && $newY >= 0 && $newY <= $this->maxY) {
+            if ($depth === 0) {
+                $this->antinodes1[] = $newX . '_' . $newY;
+            }
+            $this->antinodes2[] = $newX . '_' . $newY;
+
+            $this->findAntinode($newX, $newY, $x1, $y1, ++$depth);
+        }
     }
 }
