6dfa3cdec595b50314f9b3a2e7b84f24d7c9e8f7
Martin Boer <Martini1987@gmail.com>
2024-12-14 23:17:31 +0100

    :sparkles: 2024-11 solution part 2

--- app/Console/Commands/Advent_2024_11.php
+++ app/Console/Commands/Advent_2024_11.php
@@ -16,42 +16,56 @@ class Advent_2024_11 extends Command
 125 17
 TEXT;
 
-    private array $stones = [];
+    /**
+     * Credits to ChatGPT for coming up with the idea of grouping similar stones, so we do not run into memory issues.
+     */
+    private array $stoneCounts = [];
 
     public function handle(): void
     {
         $data = $this->option('stdin') ? file_get_contents('php://stdin') : $this->data;
-        $this->stones = array_map('intval', explode(' ', $data));
+        $stones = array_map('intval', explode(' ', $data));
+        $this->stoneCounts = array_count_values($stones);
 
         for ($i = 0; $i < 25; $i++) {
             $this->blink();
         }
 
-        $this->info('Amount of stones after blinking 25 times: ' . count($this->stones));
+        $this->info('Amount of stones after blinking 25 times: ' . array_sum($this->stoneCounts));
+
+        for ($i = 0; $i < 50; $i++) {
+            $this->blink();
+        }
+
+        $this->info('Amount of stones after blinking 75 times: ' . array_sum($this->stoneCounts));
     }
 
     private function blink(): void
     {
-        $newStones = [];
+        $newCounts = [];
 
-        foreach ($this->stones as $stone) {
+        foreach ($this->stoneCounts as $stone => $count) {
             if ($stone === 0) {
-                $newStones[] = 1;
+                $newCounts[1] = ($newCounts[1] ?? 0) + $count;
                 continue;
             }
 
             $length = (int) floor(log10($stone) + 1);
             if ($length % 2 === 0) {
                 $stoneString = (string) $stone;
-                $newStones[] = (int) substr($stoneString, 0, $length / 2);
-                $newStones[] = (int) substr($stoneString, $length / 2);
+                $part1 = (int) substr($stoneString, 0, $length / 2);
+                $part2 = (int) substr($stoneString, $length / 2);
+
+                $newCounts[$part1] = ($newCounts[$part1] ?? 0) + $count;
+                $newCounts[$part2] = ($newCounts[$part2] ?? 0) + $count;
             } else {
-                $newStones[] = $stone * 2024;
+                $newStone = $stone * 2024;
+                $newCounts[$newStone] = ($newCounts[$newStone] ?? 0) + $count;
             }
         }
 
-        $this->option('debug') && $this->comment(implode(' ', $newStones));
+        $this->option('debug') && $this->comment(var_export($newCounts, true));
 
-        $this->stones = $newStones;
+        $this->stoneCounts = $newCounts;
     }
 }
