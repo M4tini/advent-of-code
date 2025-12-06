64daa3f0cc862efe43cd11e236bd9a1614441ca7
Martin Boer <Martini1987@gmail.com>
2025-12-06 23:17:08 +0100

    :construction: 2025-05 part 2 (works with example but not with puzzle input)

--- app/Console/Commands/Advent_2025_05.php
+++ app/Console/Commands/Advent_2025_05.php
@@ -24,12 +24,15 @@ class Advent_2025_05 extends Command
 32
 TEXT;
 
+    private array $processedRanges = [];
+
     public function handle(): void
     {
         $data = $this->option('stdin') ? file_get_contents('php://stdin') : $this->data;
         [$ingredientRangeData, $ingredientIdData] = explode(PHP_EOL . PHP_EOL, $data);
         $ingredientRanges = collect(explode(PHP_EOL, $ingredientRangeData))
-            ->map(fn (string $ingredientRange) => explode('-', $ingredientRange));
+            ->map(fn (string $ingredientRange) => array_map('intval', explode('-', $ingredientRange)))
+            ->sortBy(fn (array $ingredientRange) => $ingredientRange[0]);
         $ingredientIds = explode(PHP_EOL, $ingredientIdData);
         $freshIngredientCount = 0;
 
@@ -42,7 +45,39 @@ TEXT;
             }
         }
 
+        // Using array_fill() causes memory limits, so we reduce the sorted ranges to a single value.
+        $freshIngredientRangeCount = $ingredientRanges->reduce(function (int $carry, array $range) {
+            foreach ($this->processedRanges as $processedIndex => $processedRange) {
+                // Early return if the new range falls completely within a processed range.
+                if ($processedRange[0] <= $range[0] && $processedRange[1] >= $range[1]) {
+                    return $carry;
+                }
+
+                // Remove the processed range if it falls completely within the new range.
+                if ($processedRange[0] >= $range[0] && $processedRange[1] <= $range[1]) {
+                    unset($processedRange[$processedIndex]);
+                    continue;
+                }
+
+                // Adjust the beginning of the new range to match a processed range if needed.
+                if ($processedRange[0] <= $range[0] && $processedRange[1] >= $range[0]) {
+                    $range[0] = $processedRange[1] + 1;
+                }
+
+                // Adjust the end of the range to match a processed range if needed.
+                if ($processedRange[0] >= $range[0] && $processedRange[1] >= $range[0]) {
+                    $range[1] = $processedRange[0] - 1;
+                }
+            }
+
+            $this->processedRanges[] = $range;
+
+            $this->info('Adding ' . $range[1] . ' - ' . $range[0] . ' = ' . ($range[1] - $range[0] + 1));
+
+            return $carry + $range[1] - $range[0] + 1;
+        }, 0);
 
         $this->info('Amount of fresh ingredients: ' . $freshIngredientCount);
+        $this->info('Amount of ingredients considered fresh: ' . $freshIngredientRangeCount); // 354179868661316 is too high
     }
 }
