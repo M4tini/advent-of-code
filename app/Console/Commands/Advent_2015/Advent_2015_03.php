a2a6c7566646da70fa04e6934a8fc471603783f4
Martin Boer <Martini1987@gmail.com>
2025-12-12 23:12:06 +0100

    :truck: move advent commands into subfolders

--- app/Console/Commands/Advent_2015/Advent_2015_03.php
+++ app/Console/Commands/Advent_2015/Advent_2015_03.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Advent_2015;
 
 use Illuminate\Console\Command;
 
--- app/Console/Commands/Advent_2024/Advent_2024_02.php
+++ app/Console/Commands/Advent_2024/Advent_2024_02.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Advent_2024;
 
 use Exception;
 use Illuminate\Console\Command;
