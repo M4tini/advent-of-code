d22cd581c09e6f9431849f9a0de5fcb29f5ffdbf
Martin Boer <Martini1987@gmail.com>
2025-12-14 01:14:05 +0100

    :construction: 2025-07 part 2 (works with example but not with puzzle input)

--- app/Console/Commands/Advent_2025/Advent_2025_07.php
+++ app/Console/Commands/Advent_2025/Advent_2025_07.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Advent_2025;
 
 use Illuminate\Console\Command;
 
@@ -29,12 +29,14 @@ class Advent_2025_07 extends Command
 ...............
 TEXT;
 
+    private int $timelines = 0;
+
     public function handle(): void
     {
         $data = $this->option('stdin') ? file_get_contents('php://stdin') : $this->data;
         $dataLines = explode(PHP_EOL, $data);
         $splitCount = 0;
-        $currentLine = array_shift($dataLines);
+        $currentLine = '';
 
         foreach ($dataLines as $nextLine) {
             $beamLocations = str_split($currentLine);
@@ -54,6 +56,32 @@ TEXT;
             $currentLine = $nextLine;
         }
 
+        $currentLine = array_shift($dataLines);
+        $currentIndex = strpos($currentLine, 'S');
+
+        // The lines which only consist of `.` are not interesting for the timeline count.
+        $dataLines = array_filter($dataLines, fn (string $dataLine) => str_contains($dataLine, '^'));
+
+        $this->findTimelines($currentIndex, $dataLines);
+
         $this->info('Beam splits: ' . $splitCount);
+        $this->info('Amount of timelines: ' . $this->timelines);
+    }
+
+    private function findTimelines(int $index, array $dataLines): void
+    {
+        $nextLine = array_shift($dataLines);
+
+        if ($nextLine === null) {
+            $this->timelines++;
+            return;
+        }
+
+        if ($nextLine[$index] === '^') {
+            $this->findTimelines($index - 1, $dataLines);
+            $this->findTimelines($index + 1, $dataLines);
+        } else {
+            $this->findTimelines($index, $dataLines);
+        }
     }
 }
